61ad5da057bc4ac19e92b1f74849bbba61a182f3
Toxicvanished <dengtia4@msu.edu>
2023-12-15 21:07:11 -0500

    Test added

---
+++ test_suite/test.py
@@ -0,0 +1,10 @@
+import pytest
+import workflow_functions
+
+sample_list = {'CONTRL': ['SCFTYP', 'MPLEVL', 'CITYP', 'CCTYP', 'RUNTYP', 'EXETYP', 'ICHARG', 'MULT', 'COORD', 'UNITS', 'ISPHER', 'MAXIT', 'NPRINT', 'ICUT'], \
+            'SYSTEM': ['MWORDS', 'MEMDDI'], 'BASIS': ['GBASIS', 'NGAUSS'], 'SCF': ['DIRSCF', 'DIIS', 'SOSCF', 'NOCONV', 'CONV', 'COUPLE', 'F', 'ALPHA', 'BETA'], \
+            'CCINP': ['MAXCC', 'NCORE', 'NFZV', 'ACP(1)', 'NACTO', 'NACTU']}
+
+def test_read_keyword_list():
+    assert workflow_functions.read_keyword_list("Category_keywords.csv") == sample_list
+
